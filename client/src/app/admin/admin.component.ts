156117802b4a5754b3cdccb2c25ecc88fe88cf56
maxspies <max.spies@mni.thm.de>
2019-01-07 13:25:06 +0100

    merge from dashboard

--- client/src/app/admin/admin.component.ts
+++ client/src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
-
-  constructor() { }
+  role: string;
+  path: string;
+  constructor( protected route: Router) { }
 
   ngOnInit() {
+    this.role = 'admin';
+    this.path = this.route.url;
   }
 
+  logout () {
+  }
 }
